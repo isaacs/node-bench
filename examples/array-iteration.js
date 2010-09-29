37b67f78c63c8eecbb462648621a7073db7fc116
isaacs <i@izs.me>
2010-09-29 09:14:05 -0700

    Update output

--- examples/array-iteration.js
+++ examples/array-iteration.js
@@ -25,45 +25,47 @@ exports.compare =
   }
 require("bench").runMain()
 
-// [21:04:29] $ node examples/array-iteration.js 
-// benchmarking /Users/isaacs/Documents/src/js/node-bench/examples/array-iteration.js
-// Please be patient.
-// Scores: (bigger is better)
-// 
-// i < l
-// Raw:
-//  > 278293.13543599256
-//  > 278551.53203342616
-//  > 271493.2126696833
-//  > 278551.53203342616
-//  > 278551.53203342616
-// Average (mean) 277088.1888411909
-// 
-// i < arr.length
-// Raw:
-//  > 176678.44522968197
-//  > 214285.7142857143
-//  > 214438.88491779842
-//  > 214285.7142857143
-//  > 213827.51247327155
-// Average (mean) 206703.25423843611
-// 
-// arr.forEach
-// Raw:
-//  > 40000
-//  > 39904.229848363924
-//  > 39936.10223642173
-//  > 39968.02557953637
-//  > 39952.05753096285
-// Average (mean) 39952.083039056975
-// 
-// Winner: i < l
-// Compared with next highest (i < arr.length), it's:
-// 25.4% faster
-// 1.34 times as fast
-// 0 order(s) of magnitude faster
-// 
-// Compared with the slowest (arr.forEach), it's:
-// 85.58% faster
-// 6.94 times as fast
-// 0 order(s) of magnitude faster
+/*
+[09:13:03] $ node array-iteration.js 
+benchmarking /Users/isaacs/Dropbox/dev/js/node-bench/examples/array-iteration.js
+Please be patient.
+Scores: (bigger is better)
+
+i < l
+Raw:
+ > 278293.13543599256
+ > 276497.69585253455
+ > 278293.13543599256
+ > 278293.13543599256
+ > 278293.13543599256
+Average (mean) 277934.04751930095
+
+i < arr.length
+Raw:
+ > 198807.15705765408
+ > 212916.96238466998
+ > 214132.76231263383
+ > 214132.76231263383
+ > 213523.13167259787
+Average (mean) 210702.5551480379
+
+arr.forEach
+Raw:
+ > 38759.68992248062
+ > 38358.26620636747
+ > 38580.246913580246
+ > 38880.24883359254
+ > 38940.80996884735
+Average (mean) 38703.85236897365
+
+Winner: i < l
+Compared with next highest (i < arr.length), it's:
+24.19% faster
+1.32 times as fast
+0.12 order(s) of magnitude faster
+
+Compared with the slowest (arr.forEach), it's:
+86.07% faster
+7.18 times as fast
+0.86 order(s) of magnitude faster
+*/
