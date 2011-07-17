83f6ac7bb15a526d7da0ccbee4b3c5994e2bddb4
isaacs <i@izs.me>
2011-07-17 16:56:00 -0700

    Output the versions.  That's important

--- examples/array-ify.js
+++ examples/array-ify.js
@@ -66,190 +66,353 @@ function sliceZeroApplyArg (x) {
 
 exports.compare =
   { manualMap: function () {
-      return manualMap(1,2,3,4,5,6,7,8,9,0)
+      return manualMap(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , manualMapArg: function() {
-      return manualMapArg(1,2,3,4,5,6,7,8,9,0)
+      return manualMapArg(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , arrayApply: function() {
-      return arrayApply(1,2,3,4,5,6,7,8,9,0)
+      return arrayApply(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , arrayApplyArg: function() {
-      return arrayApplyArg(1,2,3,4,5,6,7,8,9,0)
+      return arrayApplyArg(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , arrayCall: function() {
-      return arrayCall(1,2,3,4,5,6,7,8,9,0)
+      return arrayCall(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , arrayCallArg: function() {
-      return arrayCallArg(1,2,3,4,5,6,7,8,9,0)
+      return arrayCallArg(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceCall: function() {
-      return sliceCall(1,2,3,4,5,6,7,8,9,0)
+      return sliceCall(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceCallArg: function() {
-      return sliceCallArg(1,2,3,4,5,6,7,8,9,0)
+      return sliceCallArg(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceZeroCall: function() {
-      return sliceZeroCall(1,2,3,4,5,6,7,8,9,0)
+      return sliceZeroCall(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceZeroCallArg: function() {
-      return sliceZeroCallArg(1,2,3,4,5,6,7,8,9,0)
+      return sliceZeroCallArg(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceApply: function() {
-      return sliceApply(1,2,3,4,5,6,7,8,9,0)
+      return sliceApply(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceApplyArg: function() {
-      return sliceApplyArg(1,2,3,4,5,6,7,8,9,0)
+      return sliceApplyArg(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceZeroApply: function() {
-      return sliceZeroApply(1,2,3,4,5,6,7,8,9,0)
+      return sliceZeroApply(Math.random(), Math.random(), Math.random(), Math.random())
     }
   , sliceZeroApplyArg: function() {
-      return sliceZeroApplyArg(1,2,3,4,5,6,7,8,9,0)
+      return sliceZeroApplyArg(Math.random(), Math.random(), Math.random(), Math.random())
     }
   }
 
 require("../").runMain()
 
 /*
+
+////
+// First, using node 0.4.9, current stable
+////
+
+$ node examples/array-ify.js
 benchmarking /Users/isaacs/dev-src/js/node-bench/examples/array-ify.js
 Please be patient.
+{ node: '0.4.9',
+  v8: '3.1.8.25',
+  ares: '1.7.4',
+  ev: '4.4',
+  openssl: '0.9.8l' }
 Scores: (bigger is better)
-
-arrayCall
-Raw:
- > 5918.081918081918
- > 5746.253746253747
- > 4681.318681318681
- > 5902.097902097902
- > 5949.050949050949
-Average (mean) 5639.36063936064
-
-arrayCallArg
-Raw:
- > 6030.969030969031
- > 5064.935064935065
- > 5755.244755244756
- > 5182.817182817183
- > 5903.096903096903
-Average (mean) 5587.4125874125875
-
-sliceCall
-Raw:
- > 5429.57042957043
- > 5375.624375624376
- > 5340.659340659341
- > 5496.5034965034965
- > 4968.031968031968
-Average (mean) 5322.077922077922
-
-sliceCallArg
-Raw:
- > 5239.7602397602395
- > 5359.64035964036
- > 5357.642357642358
- > 5398.601398601399
- > 5072.927072927073
-Average (mean) 5285.714285714285
-
-sliceZeroCall
-Raw:
- > 5553.4465534465535
- > 5338.661338661339
- > 5342.657342657342
- > 4795.204795204795
- > 5142.857142857143
-Average (mean) 5234.565434565435
-
-sliceZeroCallArg
-Raw:
- > 5431.568431568431
- > 5367.632367632367
- > 4291.708291708292
- > 5346.6533466533465
- > 5464.535464535465
-Average (mean) 5180.41958041958
 
 arrayApply
 Raw:
- > 4832.167832167832
- > 4873.126873126873
- > 4656.343656343656
- > 4287.712287712287
- > 4534.465534465535
-Average (mean) 4636.763236763237
-
-sliceApplyArg
-Raw:
- > 5003.996003996004
- > 4893.106893106893
- > 4930.06993006993
- > 4518.481518481519
- > 3457.5424575424577
-Average (mean) 4560.639360639361
+ > 4811.188811188811
+ > 4846.153846153846
+ > 4843.156843156843
+ > 4824.175824175824
+ > 4777.222777222777
+Average (mean) 4820.379620379621
 
 arrayApplyArg
 Raw:
- > 5000.999000999001
- > 4805.194805194805
- > 4809.190809190809
- > 3196.803196803197
- > 4927.072927072927
-Average (mean) 4547.852147852148
+ > 4683.316683316683
+ > 4799.200799200799
+ > 4639.36063936064
+ > 4754.245754245754
+ > 4652.347652347652
+Average (mean) 4705.694305694306
+
+sliceCallArg
+Raw:
+ > 4466.533466533467
+ > 4502.497502497503
+ > 4478.5214785214785
+ > 4468.531468531469
+ > 4415.584415584415
+Average (mean) 4466.333666333667
+
+sliceZeroCallArg
+Raw:
+ > 4543.456543456544
+ > 4481.518481518481
+ > 4503.4965034965035
+ > 4262.737262737262
+ > 4375.624375624376
+Average (mean) 4433.366633366632
+
+sliceCall
+Raw:
+ > 4505.494505494506
+ > 4485.514485514485
+ > 4340.659340659341
+ > 4409.59040959041
+ > 4187.812187812187
+Average (mean) 4385.814185814187
+
+arrayCallArg
+Raw:
+ > 4436.563436563437
+ > 4373.626373626374
+ > 4388.611388611389
+ > 4313.686313686314
+ > 4377.622377622378
+Average (mean) 4378.021978021979
+
+sliceZeroCall
+Raw:
+ > 4526.473526473526
+ > 4143.856143856144
+ > 4484.515484515485
+ > 4215.784215784216
+ > 4389.61038961039
+Average (mean) 4352.047952047951
+
+arrayCall
+Raw:
+ > 4482.517482517483
+ > 4339.66033966034
+ > 4395.604395604396
+ > 4208.791208791209
+ > 4254.745254745255
+Average (mean) 4336.263736263737
+
+sliceApplyArg
+Raw:
+ > 4161.838161838162
+ > 4142.857142857143
+ > 4105.894105894105
+ > 4091.908091908092
+ > 4107.892107892108
+Average (mean) 4122.077922077922
 
 sliceApply
 Raw:
- > 4996.003996003996
- > 4050.949050949051
- > 4600.399600399601
- > 5046.953046953047
- > 3790.2097902097903
-Average (mean) 4496.903096903096
-
-sliceZeroApplyArg
-Raw:
- > 4186.813186813187
- > 4186.813186813187
- > 4238.761238761239
- > 3416.5834165834167
- > 4245.754245754246
-Average (mean) 4054.945054945055
+ > 4107.892107892108
+ > 4078.921078921079
+ > 4044.9550449550447
+ > 3452.5474525474524
+ > 3902.097902097902
+Average (mean) 3917.2827172827174
 
 sliceZeroApply
 Raw:
- > 4419.580419580419
- > 3518.4815184815184
- > 4240.759240759241
- > 4274.725274725275
- > 3201.7982017982017
-Average (mean) 3931.0689310689313
+ > 3865.1348651348653
+ > 3796.203796203796
+ > 3842.157842157842
+ > 3789.2107892107892
+ > 3508.4915084915083
+Average (mean) 3760.23976023976
+
+sliceZeroApplyArg
+Raw:
+ > 3818.181818181818
+ > 3824.1758241758243
+ > 3817.182817182817
+ > 3797.2027972027972
+ > 3463.5364635364635
+Average (mean) 3744.0559440559437
 
 manualMap
 Raw:
- > 683.6327345309381
- > 692.6147704590818
- > 693.3066933066933
- > 688.622754491018
- > 653.692614770459
-Average (mean) 682.3739135116381
+ > 1350.6493506493507
+ > 1386.6133866133866
+ > 1382.6173826173826
+ > 1383.6163836163837
+ > 1368.6313686313686
+Average (mean) 1374.4255744255747
 
 manualMapArg
 Raw:
- > 662.3376623376623
- > 673.3266733266734
- > 673.3266733266734
- > 564.8702594810379
- > 666.6666666666666
-Average (mean) 648.1055870277427
+ > 1345.6543456543457
+ > 1343.6563436563436
+ > 1343.6563436563436
+ > 1333.6663336663337
+ > 1343.6563436563436
+Average (mean) 1342.057942057942
 
-Winner: arrayCall
-Compared with next highest (arrayCallArg), it's:
-0.92% faster
-1.01 times as fast
-0 order(s) of magnitude faster
+Winner: arrayApply
+Compared with next highest (arrayApplyArg), it's:
+2.38% faster
+1.02 times as fast
+0.01 order(s) of magnitude faster
 
 Compared with the slowest (manualMapArg), it's:
-88.51% faster
-8.7 times as fast
-0.94 order(s) of magnitude faster
+72.16% faster
+3.59 times as fast
+0.56 order(s) of magnitude faster
+
+////
+// Now with crankshaft, node master
+////
+
+benchmarking /Users/isaacs/dev-src/js/node-bench/examples/array-ify.js
+Please be patient.
+{ node: '0.5.2-pre',
+  v8: '3.4.12.1',
+  ares: '1.7.4',
+  uv: '0.1',
+  openssl: '0.9.8l' }
+Scores: (bigger is better)
+
+arrayApply
+Raw:
+ > 7719.2807192807195
+ > 7851.1488511488515
+ > 7419.580419580419
+ > 6841.158841158841
+ > 4435.129740518962
+Average (mean) 6853.259714337558
+
+arrayApplyArg
+Raw:
+ > 6442.557442557442
+ > 5976.166832174777
+ > 6257.742257742258
+ > 6822.177822177822
+ > 5379.62037962038
+Average (mean) 6175.652946854536
+
+sliceZeroApply
+Raw:
+ > 2344.655344655345
+ > 1992.007992007992
+ > 1471.5284715284715
+ > 2244.755244755245
+ > 1734.0637450199204
+Average (mean) 1957.4021595933948
+
+arrayCall
+Raw:
+ > 2296.111665004985
+ > 1717.2827172827174
+ > 2033.966033966034
+ > 1719.840478564307
+ > 1358.8469184890655
+Average (mean) 1825.2095626614216
+
+sliceCall
+Raw:
+ > 1558.4415584415585
+ > 1613.3866133866134
+ > 1856.430707876371
+ > 1306.6933066933068
+ > 2017.982017982018
+Average (mean) 1670.5868408759736
+
+sliceZeroCall
+Raw:
+ > 1862.1378621378622
+ > 1616.7664670658683
+ > 1920.0799200799202
+ > 1183.8161838161839
+ > 1742.2577422577422
+Average (mean) 1665.0116350715155
+
+sliceApply
+Raw:
+ > 1554.4455544455545
+ > 1078.921078921079
+ > 1133.8661338661339
+ > 1404.5954045954045
+ > 1074.925074925075
+Average (mean) 1249.3506493506495
+
+manualMap
+Raw:
+ > 800.1998001998002
+ > 859.1408591408591
+ > 835.1648351648352
+ > 830.1698301698302
+ > 826.1738261738262
+Average (mean) 830.1698301698301
+
+arrayCallArg
+Raw:
+ > 422.1556886227545
+ > 413.5864135864136
+ > 424.57542457542456
+ > 417.33067729083666
+ > 410.5894105894106
+Average (mean) 417.64752293296806
+
+sliceZeroCallArg
+Raw:
+ > 137.45019920318725
+ > 128.7425149700599
+ > 126.87312687312688
+ > 127.72277227722772
+ > 124.75442043222004
+Average (mean) 129.10860675116436
+
+sliceZeroApplyArg
+Raw:
+ > 126.49402390438247
+ > 122.1449851042701
+ > 123.87612387612387
+ > 123.87612387612387
+ > 126.59470068694799
+Average (mean) 124.59719148956965
+
+sliceCallArg
+Raw:
+ > 126.49402390438247
+ > 122.50996015936255
+ > 125.62313060817547
+ > 113.32007952286283
+ > 124.2544731610338
+Average (mean) 122.44033347116343
+
+sliceApplyArg
+Raw:
+ > 123.50597609561753
+ > 124.37810945273633
+ > 124.87512487512487
+ > 89.82035928143712
+ > 115.42288557213931
+Average (mean) 115.60049105541103
+
+manualMapArg
+Raw:
+ > 113.32007952286283
+ > 108.34990059642148
+ > 108.16125860373648
+ > 109.67098703888335
+ > 107.67696909272183
+Average (mean) 109.43583897092519
+
+Winner: arrayApply
+Compared with next highest (arrayApplyArg), it's:
+9.89% faster
+1.11 times as fast
+0.05 order(s) of magnitude faster
+
+Compared with the slowest (manualMapArg), it's:
+98.4% faster
+62.62 times as fast
+1.8 order(s) of magnitude faster
+
+
 */
